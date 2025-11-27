44da90765fcdd95a11f3f8e487a80c2bd93aed2c
sara <saramohsen0901@gmail.com>
2025-11-27 19:53:43 +0200

    delete app.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage3D from './pages/HomePage3D'; 
 import ProgramesCards from './pages/ProgramesCards';
 import { Navbar } from './components/Navbar'
-// import './App.css'
+
 
 function App() {
   return (
